25c18366dc82241677e8cbb8bc42463b504b9a6c
杨帆 <imqqyangfan@gmail.com>
2017-04-15 21:37:00 +0800

    new API

--- NeccWxApi/Controllers/UserController.cs
+++ NeccWxApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NeccWxApi.Controllers
 {
@@ -11,5 +12,161 @@ namespace NeccWxApi.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="localProvince">生源地</param>
+        /// <param name="account">账号</param>
+        /// <param name="password">密码</param>
+        /// <returns>登录结果</returns>
+        [HttpGet("Login&lp={localProvince}&a={account}&p={password}")]
+        public string Login(string localProvince, string account, string password)
+        {
+            try
+            {
+                var addr = Request.HttpContext.Connection.RemoteIpAddress;
+                DBLink.Log("用户" + addr.MapToIPv4() + "接入接口[登录]");
+                var re = UserServer.Login(localProvince, account, password);
+                DBLink.Log("用户" + addr.MapToIPv4() + "退出");
+                return re;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="active">秘钥</param>
+        /// <param name="account">账号</param>
+        /// <param name="password">密码</param>
+        /// <param name="phoneNum">电话</param>
+        /// <returns>注册结果</returns>
+        [HttpGet("Register&c={active}&a={account}&p={password}&n={phoneNum}")]
+        public string Register(string active, string account, string password, string phoneNum)
+        {
+            try
+            {
+                var addr = Request.HttpContext.Connection.RemoteIpAddress;
+                DBLink.Log("用户" + addr.MapToIPv4() + "接入接口[注册]");
+                if (UserServer.AccountIsExist(account).Equals("已存在"))
+                {
+                    return "账号已存在";
+                }
+
+                if (UserServer.ActiveCodeState(active).Equals("秘钥不可用")
+                    || UserServer.ActiveCodeState(active).Equals("秘钥不存在"))
+                {
+                    return "秘钥不存在或者不可用";
+                }
+
+                var re = UserServer.Register(active, account, password, phoneNum);
+                DBLink.Log("用户" + addr.MapToIPv4() + "退出");
+                return re;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// 判断账号是否存在
+        /// </summary>
+        /// <param name="account">账号</param>
+        /// <returns>结果</returns>
+        [HttpGet("AccountIsExist&a={account}")]
+        public string AccountIsExist(string account)
+        {
+            try
+            {
+                var addr = Request.HttpContext.Connection.RemoteIpAddress;
+                DBLink.Log("用户" + addr.MapToIPv4() + "接入接口[判断账号存在]");
+                var re = UserServer.AccountIsExist(account);
+                DBLink.Log("用户" + addr.MapToIPv4() + "退出");
+                return re;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// 判断秘钥状态
+        /// </summary>
+        /// <param name="activeCode">秘钥</param>
+        /// <returns>结果</returns>
+        [HttpGet("ActiveCodeState&a={activeCode}")]
+        public string ActiveCodeState(string activeCode)
+        {
+            try
+            {
+                var addr = Request.HttpContext.Connection.RemoteIpAddress;
+                DBLink.Log("用户" + addr.MapToIPv4() + "接入接口[判断秘钥状态]");
+                var re = UserServer.ActiveCodeState(activeCode);
+                DBLink.Log("用户" + addr.MapToIPv4() + "退出");
+                return re;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        /// <param name="account">账号</param>
+        /// <param name="newPassword">密码</param>
+        /// <returns>修改结果</returns>
+        [HttpGet("ModifyPassword&a={account}&np={newPassword}")]
+        public string ModifyPassowrd(string account, string newPassword)
+        {
+            try
+            {
+                var addr = Request.HttpContext.Connection.RemoteIpAddress;
+                if (!UserServer.AccountIsExist(account).Equals("已存在"))
+                {
+                    return "账号不存在";
+                }
+                DBLink.Log("用户" + addr.MapToIPv4() + "接入接口[修改密码]");
+                var re = UserServer.ModifyPassowrd(account, newPassword);
+                DBLink.Log("用户" + addr.MapToIPv4() + "退出");
+                return re;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// 查看用户
+        /// </summary>
+        /// <param name="account">用户账号</param>
+        /// <returns>用户信息</returns>
+        [HttpGet("GetUser&a={account}")]
+        public object GetUser(string account)
+        {
+            try
+            {
+                var addr = Request.HttpContext.Connection.RemoteIpAddress;
+                if (!UserServer.AccountIsExist(account).Equals("已存在"))
+                {
+                    return "账号不存在";
+                }
+                DBLink.Log("用户" + addr.MapToIPv4() + "接入接口[查看用户]");
+                var re = UserServer.GetUser(account);
+                DBLink.Log("用户" + addr.MapToIPv4() + "退出");
+                return re;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
     }
 }
--- NeccWxApi/Servers/UserServer.cs
+++ NeccWxApi/Servers/UserServer.cs
@@ -1,7 +1,254 @@
-﻿namespace NeccWxApi
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace NeccWxApi
 {
     public class UserServer
     {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="localProvince">生源地</param>
+        /// <param name="account">账号</param>
+        /// <param name="password">密码</param>
+        /// <returns>登录结果</returns>
+        public static string Login(string localProvince, string account, string password)
+        {
+            string re;
 
+            DBLink.Log("开始连接");
+
+            var con = DBLink.Connect();
+
+            if (con.State != ConnectionState.Open)
+            {
+                DBLink.Log("连接未打开");
+
+                return null;
+            }
+
+            var sqlStr = "SELECT password FROM UserAccount " +
+                         "WHERE account = '" + account + "' AND province = '" + localProvince + "' ";
+
+            var sc = new SqlCommand(sqlStr, con);
+
+            DBLink.Log("开始查询 : 语句为" + sqlStr);
+
+            sc.ExecuteNonQuery();
+
+            var reader = sc.ExecuteReader();
+
+            if (reader.Read())
+            {
+                re = ((string) reader[0]).Equals(password) ? "登录成功" : "密码错误";
+            }
+            else
+            {
+                re = "生源地或者账号错误";
+            }
+
+            DBLink.DisConnect(con);
+
+            DBLink.Log("连接关闭");
+
+            return re;
+        }
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="active">秘钥</param>
+        /// <param name="account">账号</param>
+        /// <param name="password">密码</param>
+        /// <param name="phoneNum">电话</param>
+        /// <returns>注册结果</returns>
+        public static string Register(string active, string account, string password, string phoneNum)
+        {
+            DBLink.Log("开始连接");
+
+            var con = DBLink.Connect();
+
+            if (con.State != ConnectionState.Open)
+            {
+                DBLink.Log("连接未打开");
+
+                return null;
+            }
+
+            var sqlStr = "UPDATE UserAccount " +
+                     "SET account = '" + account + "' , " +
+                     "password = '" + password + "' , " +
+                     "phoneNum = '" + phoneNum + "' , " +
+                     "activeState = 0 " +
+                     "WHERE active = '" + active + "' ";
+            var sc = new SqlCommand(sqlStr, con);
+
+            var i = sc.ExecuteNonQuery();
+
+            var re = i == 1 ? "成功" : "失败";
+
+            DBLink.DisConnect(con);
+
+            DBLink.Log("连接关闭");
+
+            return re;
+        }
+
+
+        /// <summary>
+        /// 判断账号是否存在
+        /// </summary>
+        /// <param name="account">账号</param>
+        /// <returns>结果</returns>
+        public static string AccountIsExist(string account)
+        {
+            DBLink.Log("开始连接");
+
+            var con = DBLink.Connect();
+
+            if (con.State != ConnectionState.Open)
+            {
+                DBLink.Log("连接未打开");
+
+                return null;
+            }
+
+            var sqlStr = "SELECT account FROM UserAccount " +
+                         "WHERE account = '" + account + "'";
+            var sc = new SqlCommand(sqlStr, con);
+            sc.ExecuteNonQuery();
+            var reader = sc.ExecuteReader();
+            var re = reader.Read() ? "已存在" : "不存在";
+            DBLink.DisConnect(con);
+
+            DBLink.Log("连接关闭");
+            return re;
+        }
+
+        /// <summary>
+        /// 判断秘钥状态
+        /// </summary>
+        /// <param name="activeCode">秘钥</param>
+        /// <returns>结果</returns>
+        public static string ActiveCodeState(string activeCode)
+        {
+            DBLink.Log("开始连接");
+
+            string re;
+            var con = DBLink.Connect();
+
+            if (con.State != ConnectionState.Open)
+            {
+                DBLink.Log("连接未打开");
+
+                return null;
+            }
+
+            var sqlStr = "SELECT activeState FROM UserAccount " +
+                         "WHERE active = '" + activeCode + "'";
+            var sc = new SqlCommand(sqlStr, con);
+            sc.ExecuteNonQuery();
+            var reader = sc.ExecuteReader();
+            if (reader.Read())
+            {
+                if ((bool)reader[0])
+                {
+                    re = "秘钥可用";
+                }
+                else
+                {
+                    re = "秘钥不可用";
+                }
+            }
+            else
+            {
+                re = "秘钥不存在";
+            }
+            DBLink.DisConnect(con);
+
+            DBLink.Log("连接关闭");
+            return re;
+        }
+
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        /// <param name="account">账号</param>
+        /// <param name="newPassword">密码</param>
+        /// <returns></returns>
+        public static string ModifyPassowrd(string account, string newPassword)
+        {
+            DBLink.Log("开始连接");
+
+            var con = DBLink.Connect();
+
+            if (con.State != ConnectionState.Open)
+            {
+                DBLink.Log("连接未打开");
+
+                return null;
+            }
+
+            var sqlStr = "UPDATE UserAccount " +
+                         "SET password = '" + newPassword + "' " +
+                         "WHERE account = '" + account + "'";
+            var sc = new SqlCommand(sqlStr, con);
+
+            var i = sc.ExecuteNonQuery();
+
+            var re = i == 1 ? "成功" : "失败";
+
+            DBLink.DisConnect(con);
+
+            DBLink.Log("连接关闭");
+
+            return re;
+        }
+
+        /// <summary>
+        /// 查看用户
+        /// </summary>
+        /// <param name="account">用户账号</param>
+        /// <returns>用户信息</returns>
+        public static object GetUser(string account)
+        {
+            DBLink.Log("开始连接");
+
+            object re;
+            var con = DBLink.Connect();
+
+            if (con.State != ConnectionState.Open)
+            {
+                DBLink.Log("连接未打开");
+
+                return null;
+            }
+
+            var sqlStr = "SELECT account , password , phoneNum , province , userType FROM UserAccount " +
+                         "WHERE account = '" + account + "'";
+            var sc = new SqlCommand(sqlStr, con);
+            sc.ExecuteNonQuery();
+            var reader = sc.ExecuteReader();
+            if (reader.Read())
+            {
+                re = new
+                {
+                    账号 = (string)reader[0],
+                    密码 = (string)reader[1],
+                    电话 = (string)reader[2],
+                    省份 = (string)reader[3],
+                    用户类型 = (string)reader[4]
+                };
+            }
+            else
+            {
+                re = "秘钥不存在";
+            }
+            DBLink.DisConnect(con);
+
+            DBLink.Log("连接关闭");
+
+            return re;
+        }
     }
 }
