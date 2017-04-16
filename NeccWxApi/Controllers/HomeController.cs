579aaa6db94bb0493211c241690382154b342e93
杨帆 <imqqyangfan@gmail.com>
2017-04-16 14:22:20 +0800

    添加ip管理,ip访问受限制

--- NeccWxApi/Controllers/HomeController.cs
+++ NeccWxApi/Controllers/HomeController.cs
@@ -21,9 +21,7 @@ namespace NeccWxApi.Controllers
         {
             var ip = Server.GetUserIp(Request.HttpContext);
 
-            var times = Server.IPHandle(ip);
-
-            return ip + " : " + times;
+            return "您的ip地址:" + ip ;
         }
     }
 }
